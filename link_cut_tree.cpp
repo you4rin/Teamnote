afc4a6e7c7f77d37350791c2d80ff40dfc912502
2019027001 <artichoke42@naver.com>
2021-08-03 21:27:14 +0900

    change link cut tree code

--- link_cut_tree.cpp
+++ link_cut_tree.cpp
@@ -1,7 +1,9 @@
-// boj.kr/10723
+// boj.kr/18861
 #include<cstdio>
 #include<vector>
+#include<set>
 #include<algorithm>
+#define inf ((int)(2e9))
 
 using namespace std;
 using ll=long long;
@@ -12,7 +14,7 @@ struct Node{
 	int val,idx;
 	pii m;
 	bool rev;
-	Node():Node(0,0){}
+	Node():Node(inf,0){}
 	Node(int val,int idx):val(val),idx(idx){
 		l=r=p=NULL;rev=false;
 		m={val,idx};
@@ -43,8 +45,8 @@ struct Node{
 	}
 	void update(){
 		m={val,idx};
-		if(l)m=max(m,l->m);
-		if(r)m=max(m,r->m);
+		if(l)m=min(m,l->m);
+		if(r)m=min(m,r->m);
 	}
 	void lazy(){
 		if(!rev)return;
@@ -56,8 +58,9 @@ struct Node{
 };
 
 vector<Node> tree;
-vector<vector<pii>> adj;
+//vector<vector<pii>> adj;
 vector<pii> edge;
+set<pii> s;
 int n,cnt;
 ll ans,cur;
 
@@ -92,6 +95,13 @@ void cut(Node* x){
 	x->update();
 }
 
+Node* getRoot(Node* x){
+	access(x);
+	while(x->l)x=x->l,x->lazy();
+	access(x);
+	return x;
+}
+
 Node* getParent(Node* x){
 	access(x);
 	if(x->l==NULL)return NULL;
@@ -117,63 +127,89 @@ pii pathQuery(Node* x,Node* y){
 	Node* l=getLCA(x,y);
 	pii ret={l->val,l->idx};
 	access(x);splay(l);
-	if(l->r)ret=max(ret,l->r->m);
+	if(l->r)ret=min(ret,l->r->m);
 	access(y);splay(l);
-	if(l->r)ret=max(ret,l->r->m);
+	if(l->r)ret=min(ret,l->r->m);
 	return ret;
 }
 
+/*
 void query(Node* x,Node* y,int val){
-	pii tmp=pathQuery(x,y);
-	int m=tmp.first,idx=tmp.second;
-	if(m<=val||!idx)return;
-	int a=edge[idx].first,b=edge[idx].second;
-	cur+=(ll)val-m;++cnt;
-	if(getParent(&tree[n+idx])==&tree[a])cut(&tree[n+idx]);
-	else cut(&tree[a]);
-	if(getParent(&tree[n+idx])==&tree[b])cut(&tree[n+idx]);
-	else cut(&tree[b]);
-	tree[n+cnt]=Node(val,cnt);
-	makeRoot(x);
-	link(&tree[n+cnt],x);
-	link(y,&tree[n+cnt]);
-	edge[cnt]={y-&tree[0],x-&tree[0]};
+pii tmp=pathQuery(x,y);
+int m=tmp.first,idx=tmp.second;
+if(m<=val||!idx)return;
+int a=edge[idx].first,b=edge[idx].second;
+cur+=(ll)val-m;++cnt;
+if(getParent(&tree[n+idx])==&tree[a])cut(&tree[n+idx]);
+else cut(&tree[a]);
+if(getParent(&tree[n+idx])==&tree[b])cut(&tree[n+idx]);
+else cut(&tree[b]);
+tree[n+cnt]=Node(val,cnt);
+makeRoot(x);
+link(&tree[n+cnt],x);
+link(y,&tree[n+cnt]);
+edge[cnt]={y-&tree[0],x-&tree[0]};
 }
+*/
 
+/*
 void dfs(int p,int idx){
-	for(auto i:adj[idx]){
-		if(i.first==p)continue;
-		dfs(idx,i.first);
-		++cnt;
-		tree[n+cnt]=Node(i.second,cnt);
-		link(&tree[n+cnt],&tree[i.first]);
-		link(&tree[idx],&tree[n+cnt]);
-		edge[cnt]={idx,i.first};
-	}
+for(auto i:adj[idx]){
+if(i.first==p)continue;
+dfs(idx,i.first);
+++cnt;
+tree[n+cnt]=Node(i.second,cnt);
+link(&tree[n+cnt],&tree[i.first]);
+link(&tree[idx],&tree[n+cnt]);
+edge[cnt]={idx,i.first};
 }
+}
+*/
 
 int main(){
-	int t,q,s,e,v;
-	for(scanf("%d",&t);t--;){
-		scanf("%d %d",&n,&q);
-		ans=cur=cnt=0;
-		tree=vector<Node>(2*n+q);
-		adj=vector<vector<pii>>(n+1);
-		edge=vector<pii>(n+q);
-		for(int i=2;i<=n;++i){
-			scanf("%d %d",&s,&v);
-			++s;
-			adj[s].push_back({i,v});
-			adj[i].push_back({s,v});
-			cur+=v;
+	int t,q,a,b,c,d;
+	scanf("%d %d",&n,&q);
+	tree=vector<Node>(n+q+1);
+	//adj=vector<vector<pii>>(n+1);
+	edge=vector<pii>(n+q);
+	while(q--){
+		scanf("%d",&a);
+		if(a==1){
+			scanf("%d %d %d",&b,&c,&d);
+			if(getRoot(&tree[b])==getRoot(&tree[c])){
+				pii ret=pathQuery(&tree[b],&tree[c]);
+				if(ret.first>=d)continue;
+				int e=edge[ret.second].first,f=edge[ret.second].second;
+				if(getParent(&tree[n+ret.second])==&tree[e])cut(&tree[n+ret.second]);
+				else cut(&tree[e]);
+				if(getParent(&tree[n+ret.second])==&tree[f])cut(&tree[n+ret.second]);
+				else cut(&tree[f]);
+				s.erase(ret);
+			}
+			++cnt;
+			tree[n+cnt]=Node(d,cnt);
+			makeRoot(&tree[b]);
+			link(&tree[n+cnt],&tree[b]);
+			link(&tree[c],&tree[n+cnt]);
+			edge[cnt]={c,b};
+			s.insert({d,cnt});
 		}
-		dfs(-1,1);
-		while(q--){
-			scanf("%d %d %d",&s,&e,&v);
-			++s;++e;
-			query(&tree[s],&tree[e],v);
-			ans^=cur;
+		else if(a==2){
+			scanf("%d",&b);
+			while(s.size()&&s.begin()->first<b){
+				pii ret=*s.begin();
+				s.erase(s.begin());
+				int e=edge[ret.second].first,f=edge[ret.second].second;
+				if(getParent(&tree[n+ret.second])==&tree[e])cut(&tree[n+ret.second]);
+				else cut(&tree[e]);
+				if(getParent(&tree[n+ret.second])==&tree[f])cut(&tree[n+ret.second]);
+				else cut(&tree[f]);
+			}
 		}
-		printf("%lld\n",ans);
+		else{
+			scanf("%d %d",&b,&c);
+			if(getRoot(&tree[b])!=getRoot(&tree[c]))printf("0\n");
+			else printf("%d\n",pathQuery(&tree[b],&tree[c]).first);
+		}
 	}
 }
