ae80988e59029745511292a6d182691f033702b0
2019027001 <artichoke42@naver.com>
2021-09-05 07:09:01 +0900

    changed to tarjan

--- scc.cpp
+++ scc.cpp
@@ -1,44 +1,49 @@
 // boj.kr/2150
 #include<cstdio>
-#include<vector>
 #include<stack>
+#include<vector>
 #include<algorithm>
 
 using namespace std;
 
 struct Node{
-	int visit;
+	int visit,comp;
 	vector<int> v;
 };
 
+Node arr[100010];
 vector<vector<int>> scc;
 stack<int> stk;
-Node arr[10010];
-Node inv[10010];
+int cnt;
 
 bool comp(vector<int>a,vector<int>b){
 	return b.size()?a[0]<b[0]:1;
 }
 
-void dfs(int idx){
-	arr[idx].visit=1;
-	for(auto i:arr[idx].v)if(!arr[i].visit)dfs(i);
+int dfs(int idx){
+	arr[idx].visit=++cnt;
+	int ret=arr[idx].visit;
 	stk.push(idx);
-}
-
-void func(int idx,int num){
-	inv[idx].visit=1;
-	scc[num].push_back(idx);
-	for(auto i:inv[idx].v)if(!inv[i].visit)func(i,num);
-}
-
-void findscc(int n){
-	for(int i=1;i<=n;++i)if(!arr[i].visit)dfs(i);
-	while(!stk.empty()){
-		scc.push_back(vector<int>());
-		func(stk.top(),scc.size()-1);
-		while(!stk.empty()&&inv[stk.top()].visit)stk.pop();
+	for(auto i:arr[idx].v){
+		if(!arr[i].visit)ret=min(ret,dfs(i));
+		else if(!arr[i].comp)ret=min(ret,arr[i].visit);
 	}
+	if(arr[idx].visit==ret){
+		vector<int> tmp;
+		while(1){
+			int t=stk.top();
+			stk.pop();
+			arr[t].comp=scc.size()+1;
+			tmp.push_back(t);
+			if(t==idx)break;
+		}
+		scc.push_back(tmp);
+	}
+	return ret;
+}
+
+void find_scc(int n){
+	for(int i=1;i<=n;++i)if(!arr[i].visit)dfs(i);
 }
 
 int main(){
@@ -47,9 +52,8 @@ int main(){
 	for(int i=0;i<m;++i){
 		scanf("%d %d",&s,&e);
 		arr[s].v.push_back(e);
-		inv[e].v.push_back(s);
 	}
-	findscc(n);
+	find_scc(n);
 	for(auto& i:scc)sort(i.begin(),i.end());
 	sort(scc.begin(),scc.end(),comp);
 	printf("%d\n",scc.size());
