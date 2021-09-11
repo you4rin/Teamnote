8c94efec9ede5c8951053b34441b230e50a9bbed
2019027001 <artichoke42@naver.com>
2021-09-11 11:39:02 +0900

    line intersection first revise

--- line_intersection.cpp
+++ line_intersection.cpp
@@ -1,4 +1,3 @@
-// boj.kr/17387
 #include<iostream>
 #include<string>
 #include<vector>
@@ -10,48 +9,42 @@ using ll=long long;
 
 struct point{
 	ll x,y;
+	bool operator==(point& other){return x==other.x&&y==other.y;}
+	bool operator<(point& other){
+		if(x==other.x)return y<other.y;
+		return x<other.x;
+	}
 };
 
 struct line{
 	point p1,p2;
+	bool operator<(line& other){
+		if(p1==other.p1)return p2<other.p2;
+		return p1<other.p1;
+	}
 };
 
 point src[2],dst[2];
 
 int ccw(point p1,point p2,point p3){
-	ll x1,y1,x2,y2,det;
-	x1=p2.x-p1.x;
-	y1=p2.y-p1.y;
-	x2=p3.x-p2.x;
-	y2=p3.y-p2.y;
-	det=x1*y2-x2*y1;
-	if(det>0)return 1;
-	if(!det) return 0;
-	return -1;
+	ll det=(p2.x-p1.x)*(p3.y-p2.y)-(p3.x-p2.x)*(p2.y-p1.y);
+	return !det?0:(det>0?1:-1);
 }
 
 bool cross(line a,line b){
 	int r1,r2;
-	if(a.p1.x>a.p2.x)swap(a.p1,a.p2);
-	else if(a.p1.x==a.p2.x&&a.p1.y>a.p2.y)swap(a.p1,a.p2);
-	if(b.p1.x>b.p2.x)swap(b.p1,b.p2);
-	else if(b.p1.x==b.p2.x&&b.p1.y>b.p2.y)swap(b.p1,b.p2);
-	if(a.p1.x>b.p1.x)swap(a,b);
-	else if(a.p1.x==b.p1.x&&a.p1.y>b.p1.y)swap(a,b);
+	if(a.p2<a.p1)swap(a.p1,a.p2);
+	if(b.p2<b.p1)swap(b.p1,b.p2);
+	if(b<a)swap(a,b);
 	r1=ccw(a.p1,a.p2,b.p1),r2=ccw(a.p1,a.p2,b.p2);
 	if(r1==r2&&r1)return 0;
 	if(r1==r2&&!r1){
-		int t1,t2,t3,t4;
 		if(a.p1.x!=a.p2.x){
-			t1=a.p1.x<b.p1.x,t2=a.p1.x<b.p2.x;
-			t3=a.p2.x<b.p1.x,t4=a.p2.x<b.p2.x;
-			if(t1==t2&&t2==t3&&t3==t4)return 0;
+			if(a.p2.x<b.p1.x)return 0;
 			return 1;
 		}
 		else{
-			t1=a.p1.y<b.p1.y,t2=a.p1.y<b.p2.y;
-			t3=a.p2.y<b.p1.y,t4=a.p2.y<b.p2.y;
-			if(t1==t2&&t2==t3&&t3==t4)return 0;
+			if(a.p2.y<b.p1.y)return 0;
 			return 1;
 		}
 	}
